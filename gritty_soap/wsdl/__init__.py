071e06fd7462dcbee311414cee6309967a5641cf
Ryan A. Rodriguez <ryarodriguez@tesla.com>
2016-09-22 17:46:13 +0000

    Fixing conflict in wsdl __init__

--- gritty_soap/wsdl/__init__.py
+++ gritty_soap/wsdl/__init__.py
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
 from gritty_soap.wsdl.wsdl import Document  # noqa
-=======
-from pyOPC.gritty_soap.wsdl.wsdl import Document  # noqa
->>>>>>> 0dd844b72326a5dc6c7b568bbf0813a6c6122aec
+
