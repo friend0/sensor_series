174156798ed49b68f0403a43b68085a8729b6b93
Ryan A. Rodriguez <ryarodriguez@tesla.com>
2016-09-22 17:46:44 +0000

    Fixing conflict in xsd __init__

--- gritty_soap/xsd/__init__.py
+++ gritty_soap/xsd/__init__.py
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 from gritty_soap.xsd.builtins import *  # noqa
 from gritty_soap.xsd.elements import *  # noqa
 from gritty_soap.xsd.types import *  # noqa
 from gritty_soap.xsd.valueobjects import *  # noqa
 from gritty_soap.xsd.schema import Schema  # noqa
 from gritty_soap.xsd.indicators import *  # noqa
-=======
-from pyOPC.gritty_soap.xsd.builtins import *  # noqa
-from pyOPC.gritty_soap.xsd.elements import *  # noqa
-from pyOPC.gritty_soap.xsd.types import *  # noqa
-from pyOPC.gritty_soap.xsd.valueobjects import *  # noqa
-from pyOPC.gritty_soap.xsd.schema import Schema  # noqa
-from pyOPC.gritty_soap.xsd.indicators import *  # noqa
->>>>>>> 0dd844b72326a5dc6c7b568bbf0813a6c6122aec
