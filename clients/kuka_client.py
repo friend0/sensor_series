43212a0a78c6bc582773e0cb4e3050dbed55e7d1
Ryan A. Rodriguez <ryarodriguez@tesla.com>
2016-10-13 21:24:55 +0000

    Minor cleanup in service calls, added docstrings to KukaClient functions.

--- clients/kuka_client.py
+++ clients/kuka_client.py
@@ -8,44 +8,97 @@ from gritty_soap import Client
 class KukaClient(BaseClient):
     """
 
-    A Kuka Client class for making for communicating with an OPC server via XML DA.
+    A Kuka Client class for communicating with an OPC server via XML DA.
 
     """
 
     robot = attr.ib(default=None)
     wsdl = attr.ib(default=None)
-    service = attr.ib(default='OpcXmlDA')
-    client = attr.ib(default=Client(wsdl="../OpcXMLDaServer.asmx", service_name=service))
+    client = attr.ib(default=Client(wsdl="../OpcXMLDaServer.asmx", service_name='OpcXmlDA'))
 
-    def status(self):
-        result = self.functions.GetStatus()
-        return result
+    def status(self, **kwargs):
+        """
+
+        Retrieve the client's status and health parameters.
+
+        Makes a direct call to the client service 'GetStatus'. You may pass in desired arguments by using kwargs.
+
+        :return:
+
+        """
+        _status = self.client.service.GetStatus
+        return _status()
 
     def browse(self, item_name='', browse_filter='all', options=None):
+        """
+
+        Browse the elements of the OPC server.
+
+        By default, will return the elements at the OPC server's root. You may also use this function to browse
+        Items in the returned elements list.
+
+        :param item_name:
+        :param browse_filter:
+        :param options:
+        :return:
+
+        """
         if not options:
             options = {'ItemName':item_name, 'BrowseFilter': browse_filter, 'LocaleID':'en-US', 'ClientRequestHandle': None,
                        'ReturnAllProperties':True, 'ReturnPropertyValues':True, 'ReturnErrorText':True}
 
-        return self.functions.Browse(**options)
+        _browse = self.client.service.Browse
+        return _browse(**options)
 
     def read(self, item_name, element='RobotVar', options=None):
+        """
+
+        Read an item from the OPC server.
+
+        By default, we read variables from the RobotVar element on the Kuka OPC server. You may access items from other
+        other elements, i.e. 'KRCReg, ' by providing 'KRCReg' to the elements keyword argument of this function.
+
+        :param item_name:
+        :param element:
+        :param options:
+        :return:
+
+        """
         if not options:
             options = {'ReturnItemTime':True, 'ReturnItemName':True}
 
-        _read = self.client.services.Read
+        _read = self.client.service.Read
         return _read(Options=options,
                             ItemList={'MaxAge':0, 'Items':{'ItemPath':'', 'ItemName':"{}.{}".format(element, item_name)}})
 
     def get_properties(self, item_name=None, item_path=None):
+        """
+
+        Retrieve properties for a specific item.
+
+        :param item_name:
+        :param item_path:
+        :return:
+        """
         if not item_name:
             item_name = 'RobotVar.TipDressCounter'
         if not item_path:
             item_path = ''
 
-        _get_properties = self.client.services.GetProperties
+        _get_properties = self.client.service.GetProperties
         return _get_properties(ReturnPropertyValues=True, ItemIDs=[{'ItemPath':item_path, 'ItemName':item_name}])
 
     def subscribe(self, item_name, server_handle, element='RobotVar', options=None):
+        """
+
+        Subscribe to updates on a particular item. Non-polling!
+
+        :param item_name:
+        :param server_handle:
+        :param element:
+        :param options:
+        :return:
+        """
         if not options:
             options = {'ReturnItemTime':True}
 
@@ -54,6 +107,17 @@ class KukaClient(BaseClient):
                           ItemList={'MaxAge': 0, 'Items': {'ItemPath': '', 'ItemName': "{}.{}".format(element, item_name)}})
 
     def polled_subscription(self, item_name, server_handle, element=None, options=None):
+        """
+
+        Setup a polled subscription to updates on a particular item.
+
+        :param item_name:
+        :param server_handle:
+        :param element:
+        :param options:
+        :return:
+
+        """
         if not options:
             options = {'ReturnItemTime':True}
         if not element:
@@ -66,6 +130,15 @@ class KukaClient(BaseClient):
                                      Options=options, ItemList={'MaxAge': 0, 'Items': {'ItemPath': '', 'ItemName': "{}.{}".format(element, item_name)}})
 
     def subscription_cancel(self, server_sub_handle, client_handle):
+        """
+
+        Cancel an existing subscription.
+        
+        :param server_sub_handle:
+        :param client_handle:
+        :return:
+
+        """
         options = {'ReturnItemTime':True}
         _subscription_cancel = self.client.services.SubscriptionCancel
         return _subscription_cancel(Options=options, ServerSubHandle=server_sub_handle,
@@ -76,6 +149,7 @@ BaseClient.register(KukaClient)
 # todo: transpose this sequence to a tests suite
 if __name__ == '__main__':
     kuka_client = KukaClient()
+
     ret_val = kuka_client.status()
     print(ret_val)
     ret_val = kuka_client.browse()
