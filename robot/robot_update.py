6a67a9c11faea0c842f0f20e49b0a294514d2099
Ryan A. Rodriguez <ryarodriguez@tesla.com>
2016-10-25 14:36:36 -0700

    Updates to robot worker thread

--- robot/robot_update.py
+++ robot/robot_update.py
@@ -1,3 +1,20 @@
 import multiprocessing
+from threading import Thread
+import time
 
-class ClientUpdate():
+class RobotUpdateWorker(Thread):
+
+    def __init__(self, robots, sampling_rate):
+        super(RobotUpdateWorker, self).__init__()
+        self.daemon = True
+        #self.queue = queue
+        self.sampling_rate = sampling_rate
+
+    def run(self):
+
+        #for robot in iter(self.queue.get, None):
+        while True:
+            for robot in self.robots:
+                result = robot.client.polled_subscription()
+                print(result)
+
